cce9360f978682cddc5a3bf7d755681019be5847
Timur Jalilov <timurjalilov1@gmail.com>
2021-09-27 17:43:17 +0100

    added button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./style.css";
 
-function App() {
-  const [startTime, setStartTime] = useState(0);
+// render App
+// something happens = event handler triggers
+// apply some logic
+// oh I need to re-render
+// tell react to rerender -> here's the value for when you do that
+let milliseconds = 0;
+let seconds = 0;
+let minutes = 0;
+const startTime = Date.now();
 
-  const timer = () => {
-    if (!startTime) {
-    }
-  };
+const convertTime = (time) => {
+  milliseconds = Math.floor(time) % 1000;
+  seconds = Math.floor(time / 1000) % 60;
+  minutes = Math.floor(time / 60000) % 60;
+
+  return `${padNumbers(minutes)}:${padNumbers(seconds)}.${padNumbers(
+    Math.floor(milliseconds / 10)
+  )}`;
+};
+
+const padNumbers = (number) => {
+  return number.toString().padStart(2, 0);
+};
+
+const App = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  console.log(elapsedTime);
+
+  useEffect(() => {
+    // setInterval(() => {
+    //   setElapsedTime(Date.now() - startTime);
+    // }, 100);
+  }, []);
+
+  // const [isPaused, setIsPaused] = useState(false);
+
+  // const handlePause = () => {};
+
+  // const handleResume = () => {};
+
+  // const handleReset = () => {};
+
   return (
     <div>
-      <div id="setTime">00:00.00</div>
+      <div id="setTime">{convertTime(elapsedTime)}</div>
 
       <div className="buttons">
-        <button className="initialLapButton" id="resetLapButton">
-          Lap
+        <button className="resetLapButton" disabled={!elapsedTime}>
+          {isRunning ? "Lap" : "Reset"}
         </button>
-        <button className="startButton" id="startStopButton">
-          Start
+        <button
+          className="startButton"
+          onClick={() => setIsRunning(!isRunning)}
+        >
+          {isRunning ? "Stop" : "Start"}
         </button>
       </div>
 
@@ -53,6 +92,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
