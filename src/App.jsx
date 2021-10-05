11e99531e328f37389c87083988e0e85bb200abb
Timur Jalilov <timurjalilov1@gmail.com>
2021-10-05 15:42:55 +0100

    added css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import "./style.css"
-import { useState } from "react"
+import { useState, useReducer } from "react"
 import DisplayTime from "./DisplayTime"
 import Buttons from "./Buttons"
 import LapTable from "./LapTable"
 import useTimer from "./useTimer"
 
+const ACTIONS = {
+  IS_RUNNING: "isRunning",
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "1":
+      return {}
+    case "2":
+      return {}
+    default:
+      return state
+  }
+}
+
 const App = () => {
   const [isRunning, setIsRunning] = useState(false)
   const [lapTimes, setLapTimes] = useState([])
 
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
   const [elapsedTime, activeLap, resetActiveLap, resetElapsedTime] = useTimer(isRunning, lapTimes)
 
   const handleResetLap = () => {
