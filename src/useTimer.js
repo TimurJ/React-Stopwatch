485f951af99af00298a2740e320c09722a91a226
Timur Jalilov <timurjalilov1@gmail.com>
2021-10-05 13:55:20 +0100

    added hooks

---
+++ src/useTimer.js
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+const useTimer = (isRunning, lapTimes) => {
+  const [elapsedTime, setElapsedTime] = useState(0)
+  const [activeLap, setActiveLap] = useState(0)
+
+  useEffect(() => {
+    if (isRunning) {
+      const startTime = Date.now() - elapsedTime
+      const lapStartTime = Date.now() - activeLap
+
+      const interval = setInterval(() => {
+        setElapsedTime(Date.now() - startTime)
+        setActiveLap(Date.now() - lapStartTime)
+      }, 10)
+
+      return () => clearInterval(interval)
+    }
+  }, [isRunning, lapTimes])
+
+  const resetActiveLap = () => {
+    setActiveLap(0)
+  }
+
+  const resetElapsedTime = () => {
+    setElapsedTime(0)
+  }
+  return [elapsedTime, activeLap, resetActiveLap, resetElapsedTime]
+}
+
+export default useTimer
